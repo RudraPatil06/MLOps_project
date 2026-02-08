3270b4292fdc4272b73136e7181d02a3420a3238
RudraPatil06 <rudrarajpatil06@gmail.com>
2026-02-08 15:30:12 -0800

    Resolved merge conflicts

--- src/utils.py
+++ src/utils.py
@@ -1,44 +1,38 @@
 import os
 import sys
-<<<<<<< HEAD
-=======
-
-import numpy as np
-import pandas as pd
->>>>>>> c4ffba8e49156847fd1b2bafcc3516771db20e64
 import dill
+import pickle
 
 from src.exception import CustomException
 
-<<<<<<< HEAD
 
 def save_object(file_path, obj):
+    """
+    Save python object using dill.
+    """
     try:
         dir_path = os.path.dirname(file_path)
         os.makedirs(dir_path, exist_ok=True)
-=======
-def save_object(file_path, obj):
-    try:
-        dir_path=os.path.dirname(file_path)
-
-        os.makedirs(dir_path,exist_ok=True)
->>>>>>> c4ffba8e49156847fd1b2bafcc3516771db20e64
 
         with open(file_path, "wb") as file_obj:
             dill.dump(obj, file_obj)
 
     except Exception as e:
-<<<<<<< HEAD
         raise CustomException(e, sys)
 
 
 def load_object(file_path):
+    """
+    Load python object. Try dill first, then pickle.
+    """
     try:
         with open(file_path, "rb") as file_obj:
             return dill.load(file_obj)
 
-    except Exception as e:
-        raise CustomException(e, sys)
-=======
-        raise CustomException(e, sys)        
->>>>>>> c4ffba8e49156847fd1b2bafcc3516771db20e64
+    except Exception:
+        try:
+            with open(file_path, "rb") as file_obj:
+                return pickle.load(file_obj)
+
+        except Exception as e:
+            raise CustomException(e, sys)
